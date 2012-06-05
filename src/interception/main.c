3e2e8cc7cb8d9e31f0cff01af4bccf1da662e5ef
wangbin579 <wangbin579@gmail.com>
2012-06-05 18:46:30 +0800

    change variables

--- src/interception/main.c
+++ src/interception/main.c
@@ -1,11 +1,12 @@
-/*                                                                                           *  tcpcopy - an online replication replication tool
+/*
+ * tcpcopy - an online replication replication tool
  *
  *  Copyright 2011 Netease, Inc.  All rights reserved.
  *  Use and distribution licensed under the BSD license.  See
  *  the LICENSE file for full text.
  *
  *  Authors:
- *      bin wang <163.beijing@gmail.com or bwang@corp.netease.com>
+ *      bin wang <wangbin579@gmail.com>
  *      bo  wang <wangbo@corp.netease.com>
  */
 
@@ -13,27 +14,24 @@
 #  include <config.h>
 #endif
 
-#include <stdlib.h>
-#include <signal.h>
+#include <xcopy.h>
 
-#include "interception.h"
-
-static void releaseResources()
+static void release_resources()
 {
-	logInfo(LOG_NOTICE,"releaseResources begin");
+	logInfo(LOG_NOTICE,"release_resources begin");
 	interception_over();
-	logInfo(LOG_NOTICE,"releaseResources end except log file");
-	endLogInfo();
+	logInfo(LOG_NOTICE,"release_resources end except log file");
+	end_log_info();
 }
 
 static void signal_handler(int sig)
 {
 	logInfo(LOG_ERR,"set signal handler:%d",sig);
 	printf("set signal handler:%d\n",sig);
-	if(SIGSEGV==sig)
+	if(SIGSEGV == sig)
 	{    
-		logInfo(LOG_ERR,"SIGSEGV error");
-		releaseResources();
+		logInfo(LOG_ERR, "SIGSEGV error");
+		release_resources();
 		/*avoid dead loop*/
 		signal(SIGSEGV, SIG_DFL);
 		kill(getpid(), sig);
@@ -45,43 +43,44 @@ static void signal_handler(int sig)
 
 static void set_signal_handler(){
 	int i=1;
-	atexit(releaseResources);
-	for(;i<SIGTTOU;i++)	
+	atexit(release_resources);
+	for(; i<SIGTTOU; i++)	
 	{
-		signal(i,signal_handler);
+		signal(i, signal_handler);
 	}
 }
 
-static int retrieveIPAddresses(const char* ips)
+static int retrieve_ip_addr(const char* ips)
 {
-	size_t len;
-	int count=0;
-	const char* split;
-	const char* p=ips;
-	char tmp[32];
-	memset(tmp,0,32);
-	uint32_t inetAddr=0;
+	size_t      len;
+	int         count=0;
+	const char  *split, *p=ips;
+	char        tmp[32];
+	uint32_t    address=0;
+
+	memset(tmp, 0, 32);
+
 	while(1)
 	{
-		split=strchr(p,':');
-		if(split!=NULL)
+		split=strchr(p, ':');
+		if(split != NULL)
 		{   
-			len=(size_t)(split-p);
+			len = (size_t)(split-p);
 		}else
 		{   
-			len=strlen(p);
+			len = strlen(p);
 		}   
-		strncpy(tmp,p,len);
-		inetAddr=inet_addr(tmp);    
-		passed_ips.ips[count++]=inetAddr;
-		if(NULL==split)
+		strncpy(tmp, p, len);
+		address=inet_addr(tmp);    
+		passed_ips.ips[count++] = address;
+		if(NULL == split)
 		{
 			break;
 		}else
 		{
-			p=split+1;
+			p = split + 1;
 		}
-		memset(tmp,0,32);
+		memset(tmp, 0, 32);
 
 	}
 	passed_ips.num=count;
@@ -90,15 +89,16 @@ static int retrieveIPAddresses(const char* ips)
 
 
 int main(int argc ,char **argv){
-	if(argc>1)
+	if(argc > 1)
 	{
-		retrieveIPAddresses(argv[1]);
+		retrieve_ip_addr(argv[1]);
 	}
-	initLogInfo();
+
+	init_log_info();
 	set_signal_handler();
 	interception_init();
 	interception_run();
+
 	return 0;
 }
-
 
