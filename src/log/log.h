29d1e480f250aa0df745905ca5bf9e63bee24a9e
skoo87 <marckywu@gmail.com>
2012-07-26 19:25:23 +0800

    add tc_log_debugN function

--- src/log/log.h
+++ src/log/log.h
@@ -1,8 +1,52 @@
 #ifndef  _LOG_H_INC
 #define  _LOG_H_INC
 
-    void log_init();
-    void log_end();
+#include "config.h"
 
-#endif  
+void log_init();
+void log_end();
+
+void log_info(int level, const char *fmt, ...);
+
+#if (TCPCOPY_DEBUG)
+
+#define tc_log_debug0(level, fmt)                                       \
+    log_info(level, (const char *) fmt)
+
+#define tc_log_debug1(level, fmt, a1)                                   \
+    log_info(level, (const char *) fmt, a1)
+
+#define tc_log_debug2(level, fmt, a1, a2)                               \
+    log_info(level, (const char *) fmt, a1, a2)
+
+#define tc_log_debug3(level, fmt, a1, a2, a3)                           \
+    log_info(level, (const char *) fmt, a1, a2, a3)
+
+#define tc_log_debug4(level, fmt, a1, a2, a3, a4)                       \
+    log_info(level, (const char *) fmt, a1, a2, a3, a4)
+
+#define tc_log_debug5(level, fmt, a1, a2, a3, a4, a5)                   \
+    log_info(level, (const char *) fmt, a1, a2, a3, a4, a5)
+
+#define tc_log_debug6(level, fmt, a1, a2, a3, a4, a5, a6)               \
+    log_info(level, (const char *) fmt, a1, a2, a3, a4, a5, a6)
+
+#define tc_log_debug7(level, fmt, a1, a2, a3, a4, a5, a6, a7)           \
+    log_info(level, (const char *) fmt, a1, a2, a3, a4, a5, a6, a7)
+
+#else
+
+#define tc_log_debug0(level, fmt)
+#define tc_log_debug1(level, fmt, a1)
+#define tc_log_debug2(level, fmt, a1, a2)
+#define tc_log_debug3(level, fmt, a1, a2, a3)
+#define tc_log_debug4(level, fmt, a1, a2, a3, a4)
+#define tc_log_debug5(level, fmt, a1, a2, a3, a4, a5)
+#define tc_log_debug6(level, fmt, a1, a2, a3, a4, a5, a6)
+#define tc_log_debug7(level, fmt, a1, a2, a3, a4, a5, a6, a7)
+
+#endif /* TCPCOPY_DEBUG */
+
+#endif /* _LOG_H_INC */
+
 
