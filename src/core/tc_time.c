6b3111e1d29b5ff54cea51dd4d49c43ec6b3ab9e
skoo87 <marckywu@gmail.com>
2012-08-17 19:26:20 +0800

    modify tc_time_update()

--- src/core/tc_time.c
+++ src/core/tc_time.c
@@ -34,7 +34,7 @@ tc_time_init(long msec)
 void
 tc_time_update()
 {
-    long            msec, cur_time_msec;
+    long            msec;
     time_t          sec;
     struct tm       tm;
     struct timeval  tv;
@@ -45,23 +45,18 @@ tc_time_update()
     msec = tv.tv_usec / 1000;
 
     tc_current_time_sec = sec;
-    cur_time_msec = sec * 1000 + msec;
+    tc_current_time_msec = sec * 1000 + msec;
 
-    if (cur_time_msec != tc_current_time_msec) {
+    tc_localtime(sec, &tm);
 
-        tc_current_time_msec = cur_time_msec;
+    sprintf(cache_err_log_time, "%4d/%02d/%02d %02d:%02d:%02d +%03d",
+            tm.tm_year, tm.tm_mon,
+            tm.tm_mday, tm.tm_hour,
+            tm.tm_min, tm.tm_sec,
+            (int) msec);
 
-        tc_localtime(sec, &tm);
-
-        sprintf(cache_err_log_time, "%4d/%02d/%02d %02d:%02d:%02d +%03d",
-                tm.tm_year, tm.tm_mon,
-                tm.tm_mday, tm.tm_hour,
-                tm.tm_min, tm.tm_sec,
-                (int) msec);
-
-        tc_current_tm = tm;
-        tc_error_log_time = cache_err_log_time;
-    }
+    tc_current_tm = tm;
+    tc_error_log_time = cache_err_log_time;
 }
 
 void
