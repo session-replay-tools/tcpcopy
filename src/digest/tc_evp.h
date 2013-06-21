cba1c86c4f9a00e98dbf8a87e5bb8f33f8ee6ded
wangbin579 <wangbin579@gmail.com>
2013-06-21 16:24:23 +0800

    add sha

---
+++ src/digest/tc_evp.h
@@ -0,0 +1,18 @@
+#ifndef  TC_EVP_INCLUDED
+#define  TC_EVP_INCLUDED
+
+
+#if (TCPCOPY_DIGEST) 
+int tc_init_digests();
+int tc_destroy_digests();
+int tc_init_sha1();
+int tc_destroy_sha1();
+int tc_sha1_digest_one(unsigned char *dest, unsigned int dest_len, 
+        const unsigned char *seed, unsigned int seed_len);
+int tc_sha1_digest_two(unsigned char *dest, unsigned int dest_len, 
+        const unsigned char *seed1, unsigned int seed1_len, 
+        const unsigned char *seed2, unsigned int seed2_len);
+#endif
+
+#endif   /* ----- #ifndef TC_EVP_INCLUDED ----- */
+
